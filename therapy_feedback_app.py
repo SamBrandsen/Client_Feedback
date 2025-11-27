fe1bab470c530ec08b01323fba574b6a34ad7a4f
Sam Brandsen <sambrandsen7@gmail.com>
2025-11-26 20:16:06 -0500

    fixed banner

--- therapy_feedback_app.py
+++ therapy_feedback_app.py
@@ -58,7 +58,7 @@ if "responses" not in st.session_state:
 # Introduction
 # -------------------------------
 st.title("Therapy Experience Reflection Tool")
-st.warning("⚠️ This is an experimental / early version of thetool .")
+#st.warning("⚠️ This is an experimental / early version of thetool .")
 st.markdown("""
 This form helps you reflect on your therapy experiences — what’s working, what feels hard, and what you might want to share. All questions are optional. You can answer as many or as few as you like. 
 
